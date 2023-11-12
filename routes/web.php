b859c54a2bfea5ec579c1c04efa7cdb592670b29
Burhan Uddin Fuad <132288323+Fuad1610006@users.noreply.github.com>
2023-11-12 21:43:48 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/register', [auth::class,'signUpForm'])->name('register');
+// Route::post('/register', [auth::class,'signUpStore'])->name('register.store');
+// Route::get('/login', [auth::class,'signInForm'])->name('login');
+// Route::post('/login', [auth::class,'signInCheck'])->name('login.check');
+// Route::get('/logout', [auth::class,'singOut'])->name('logOut');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->name('dashboard');
+
