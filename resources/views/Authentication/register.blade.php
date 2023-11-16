dd298ef77c7ea247d312f75d601757c4c4e2ebd8
-- <-->
2023-11-16 10:05:03 +0600

    authentication done

--- resources/views/Authentication/register.blade.php
+++ resources/views/Authentication/register.blade.php
@@ -29,12 +29,22 @@
                       <label for="FullName" class="form-label">Your Name</label>
                       <input type="text" name="FullName" class="form-control" id="FullName" value="{{old('FullName')}}" required>
                       <div class="invalid-feedback">Please, enter your name!</div>
+                      @if($errors->has('FullName'))
+														<small class="d-block text-danger">
+															{{$errors->first('FullName')}}
+														</small>
+													@endif
                     </div>
 
                     <div class="col-12">
                       <label for="EmailAddress" class="form-label">Your Email</label>
                       <input type="email" name="EmailAddress" class="form-control" id="EmailAddress" value="{{old('EmailAddress')}}" required>
                       <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+                      @if($errors->has('EmailAddress'))
+														<small class="d-block text-danger">
+															{{$errors->first('EmailAddress')}}
+														</small>
+													@endif
                     </div>
 
                     <div class="col-12">
@@ -42,6 +52,11 @@
                       <div class="input-group has-validation">
                         <input type="text" name="contact_no_en" class="form-control" id="contact_no_en" value="{{old('contact_no_en')}}" required>
                         <div class="invalid-feedback">Please choose a username.</div>
+                        @if($errors->has('contact_no_en'))
+														<small class="d-block text-danger">
+															{{$errors->first('contact_no_en')}}
+														</small>
+													@endif
                       </div>
                     </div>
 
@@ -49,6 +64,11 @@
                       <label for="password" class="form-label">Password</label>
                       <input type="password" name="password" class="form-control" id="password" required>
                       <div class="invalid-feedback">Please enter your password!</div>
+                      @if($errors->has('password'))
+														<small class="d-block text-danger">
+															{{$errors->first('password')}}
+														</small>
+													@endif
                     </div>
 
                     <div class="col-12">
